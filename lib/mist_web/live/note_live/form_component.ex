d8d27e250945e41146c9063901bf25516b81ac95
jurraca <jurraca@users.noreply.github.com>
2025-05-23 10:01:10 +0000

    add note creation functionality with modal form

---
+++ lib/mist_web/live/note_live/form_component.ex
@@ -0,0 +1,62 @@
+
+defmodule MistWeb.NoteLive.FormComponent do
+  use MistWeb, :live_component
+
+  alias Mist.Nostr.Event
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.header>
+        <%= @title %>
+        <:subtitle>Write</:subtitle>
+      </.header>
+
+      <.simple_form
+        for={@form}
+        id="note-form"
+        phx-target={@myself}
+        phx-submit="save"
+      >
+        <.input field={@form[:content]} type="textarea" label="Content" />
+        <:actions>
+          <.button phx-disable-with="Saving...">Save Note</.button>
+        </:actions>
+      </.simple_form>
+    </div>
+    """
+  end
+
+  @impl true
+  def update(%{note: note} = assigns, socket) do
+    changeset = Event.change_note(note)
+
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign_form(changeset)}
+  end
+
+  @impl true
+  def handle_event("save", %{"note" => note_params}, socket) do
+    case Event.create_note(note_params) do
+      {:ok, note} ->
+        notify_parent({:saved, note})
+
+        {:noreply,
+         socket
+         |> put_flash(:info, "Note created successfully")
+         |> push_patch(to: socket.assigns.patch)}
+
+      {:error, changeset} ->
+        {:noreply, assign_form(socket, changeset)}
+    end
+  end
+
+  defp assign_form(socket, changeset) do
+    assign(socket, :form, to_form(changeset))
+  end
+
+  defp notify_parent(msg), do: send(self(), {__MODULE__, msg})
+end
--- lib/mist_web/live/note_live/index.ex
+++ lib/mist_web/live/note_live/index.ex
@@ -1,11 +1,30 @@
 defmodule MistWeb.NoteLive.Index do
   use MistWeb, :live_view
 
+  alias Mist.Nostr.Event
+
   @impl true
   def mount(_params, _session, socket) do
     if connected?(socket) do
       {:ok, stream(socket, :notes, [])}
     end
+  end
+
+  @impl true
+  def handle_params(params, _url, socket) do
+    {:noreply, apply_action(socket, socket.assigns.live_action, params)}
+  end
+
+  defp apply_action(socket, :index, _params) do
+    socket
+    |> assign(:page_title, "Listing Notes")
+    |> assign(:note, nil)
+  end
+
+  defp apply_action(socket, :new, _params) do
+    socket
+    |> assign(:page_title, "New Note")
+    |> assign(:note, %Event{})
   end
 
   @impl true
@@ -15,6 +34,10 @@ defmodule MistWeb.NoteLive.Index do
       pubkey: note.pubkey,
       content: note.content
     })}
+  end
+
+  def handle_info({MistWeb.NoteLive.FormComponent, {:saved, note}}, socket) do
+    {:noreply, stream_insert(socket, :notes, note)}
   end
 
   def handle_info(_, socket), do: {:noreply, socket}
