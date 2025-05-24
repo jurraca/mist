7e2c0cc81bdb5d761fae251bf39020fa56e0007d
jurraca <julienu@pm.me>
2025-05-24 18:14:13 +0200

    add livedebugger

--- mix.exs
+++ mix.exs
@@ -56,7 +56,8 @@ defmodule Mist.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:nostrbase, github: "jurraca/nostrbase"}
+      {:nostrbase, github: "jurraca/nostrbase"},
+      {:live_debugger, "~> 0.2.0", only: :dev}
     ]
   end
 
