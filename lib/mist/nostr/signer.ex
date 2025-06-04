a614672978860ef5a8e50e02b7ac1994c0e85023
jurraca <julienu@pm.me>
2025-06-04 12:52:43 +0200

    signer: set public key on signer startup; explicit impl GenServer

--- lib/mist/nostr/signer.ex
+++ lib/mist/nostr/signer.ex
@@ -14,8 +14,7 @@ defmodule Mist.Nostr.Signer do
     config = Application.get_env(:mist, :signer, mode: {:local})
 
     case config[:mode] do
-      {:local} ->
-        {:ok, %{mode: :local, authorized_clients: %{}}}
+      {:local} -> {:ok, %{mode: :local, authorized_clients: %{}}, {:continue, nil}}
 
       {:remote, bunker_url} when is_binary(bunker_url) ->
         {:ok, %{mode: :remote, bunker_url: bunker_url, authorized_clients: %{}}}
@@ -39,21 +38,33 @@ defmodule Mist.Nostr.Signer do
   end
 
   # Server callbacks
-  @impl true
+  @impl GenServer
+  def handle_continue(_arg, state) do
+    case Keys.derive_public_key() do
+      {:ok, pubkey} ->
+          :persistent_term.put(:my_profile_pubkey, pubkey)
+          {:noreply, state}
+      _ -> {:noreply, state}
+    end
+  end
+
+  @impl GenServer
   def handle_call({:connect, client_pubkey, _secret}, _from, state) do
     # In local mode, we auto-approve all connections
     {:reply, {:ok, "ack"}, put_in(state.authorized_clients[client_pubkey], true)}
   end
 
-  @impl true
+  @impl GenServer
   def handle_call(:get_public_key, _from, state) do
     case Keys.derive_public_key() do
-      {:ok, pubkey} -> {:reply, {:ok, pubkey}, state}
+      {:ok, pubkey} ->
+        {:reply, {:ok, pubkey}, state}
+
       {:error, _} = err -> {:reply, err, state}
     end
   end
 
-  @impl true
+  @impl GenServer
   def handle_call({:sign_event, event_params}, _from, %{mode: :local} = state) do
     case Keys.get_private_key() do
       {:ok, priv_key} ->
