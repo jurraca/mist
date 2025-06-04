1c157c6904904e293c04ee7988dc1f302bed645b
jurraca <julienu@pm.me>
2025-06-04 16:42:57 +0200

    migration: create nip-65 user relays table

---
+++ priv/repo/migrations/20250604143822_create_user_relays.exs
@@ -0,0 +1,15 @@
+defmodule Mist.Repo.Migrations.CreateUserRelays do
+  use Ecto.Migration
+
+  def change do
+    create table(:user_relays) do
+      add :purpose, :string, null: false
+      add :pubkey_id, references(:profiles, on_delete: :delete_all), null: false
+      add :relay_id, references(:relays, on_delete: :delete_all), null: false
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:user_relays, [:pubkey_id, :relay_id], unique: true)
+  end
+end
