8dfdb41928a29fdf471eda2bed6324fcadb8b261
jurraca <julienu@pm.me>
2025-06-08 18:37:30 +0200

    events: add changeset validations

--- lib/mist/nostr/event.ex
+++ lib/mist/nostr/event.ex
@@ -19,6 +19,16 @@ defmodule Mist.Nostr.Event do
     event
     |> cast(attrs, [:event_id, :pubkey, :created_at, :kind, :tags, :content, :sig])
     |> validate_required([:event_id, :pubkey, :created_at, :kind, :tags, :content, :sig])
+    |> validate_length(:pubkey, is: 64)
+    |> validate_length(:event_id, is: 64)
+    |> validate_length(:sig, is: 128)
+    |> validate_number(:kind, greater_than_or_equal_to: 0, less_than: 65535)
     |> unique_constraint([:event_id])
+  end
+
+  def format_attrs(%{"id" => event_id, "content" => content} = attrs) do
+    attrs
+    |> Map.put("event_id", event_id)
+    |> Map.update!("content", fn x -> if(content == "", do: nil, else: content) end)
   end
 end
